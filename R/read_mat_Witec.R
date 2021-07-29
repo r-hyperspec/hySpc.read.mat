9c792d9b964535aa8457a1c74628c23094fe1217
GregoryHuff <69199128+GregoryHuff@users.noreply.github.com>
2021-07-29 13:19:39 +0200

    make this work for non-DSO files and access wavelength units
    tested with both DSO and non-DSO as well as two different units

--- R/read_mat_Witec.R
+++ R/read_mat_Witec.R
@@ -30,13 +30,30 @@ read_mat_Witec <- function(file = stop("filename or connection needed")) {
   spcname <- names(data)
   #peel off outer layer of data structure, which only contains filename
   data <- data[[1]]
+  #WITec software can export Matlab files in two different formats
+  #the first is the so called DSO format, the other they simply call the Matlab format
+  #The DSO format has more fields, many of which are empty
+  #the names of the fields are not imported correctly in R ("axisscale" for example) when using the non-DSO format
 
-  spc <- new("hyperSpec", spc = data$data)
-
-  spc$spcname <- spcname
-  spc@wavelength<-as.vector(data[["axisscale"]][[2]][[1]]) #get wavelengths
-
-  ## consistent file import behaviour across import functions
+  #check whether it is DSO or not
+  DSOversion <- data[["datasetversion"]] #this will be NULL for non-DSO files
+  if (!is.null(DSOversion))
+  {
+    spc <- new("hyperSpec", spc = data$data)
+    spc$spcname <- spcname
+    spc@wavelength <- as.vector(data[["axisscale"]][[2]][[1]])  #get wavelengths
+    spc@label$.wavelength <- data[["axisscale"]][[4]][[1]][[1]] #get units for x-axis
+  }
+  else
+  {
+    #non-DSO file
+    #data is found in [[3]]
+    spc <-  new("hyperSpec", spc = data[[3]])
+    #axisscale is [[4]]
+    spc@wavelength<-as.vector(data[[4]][[2]][[1]])    #get wavelengths
+    spc@label$.wavelength <- data[[4]][[4]][[1]][[1]] #get units for x-axis
+  }
+  ## consistent file import behavior across import functions
   .spc_io_postprocess_optional(spc, file)
 }
 
