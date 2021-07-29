d0a7946070d6d399c3c0265ac1d892227f5b561b
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-29 15:27:42 +0300

    Fix code style
    Apply `styler` and do some manual corrections.

--- R/read_mat_Witec.R
+++ R/read_mat_Witec.R
@@ -31,28 +31,27 @@ read_mat_Witec <- function(file = stop("filename or connection needed")) {
   }
   spcname <- names(data)
   data <- data[[1]]
-  #WITec software can export Matlab files in two different formats
-  #the first is the so called DSO format, the other they simply call the Matlab format
-  #The DSO format has more fields, many of which are empty
-  #the names of the fields are not imported correctly in R ("axisscale" for example) when using the non-DSO format
+  # WITec software can export Matlab files in two different formats the first
+  # is the so called DSO format, the other they simply call the Matlab format.
+  # The DSO format has more fields, many of which are empty.
+  # The names of the fields are not imported correctly in R ("axisscale"
+  # for example) when using the non-DSO format.
 
-  #check whether it is DSO or not
-  DSOversion <- data[["datasetversion"]] #this will be NULL for non-DSO files
-  if (!is.null(DSOversion))
-  {
+  # Check whether it is DSO or not
+  DSOversion <- data[["datasetversion"]] # this will be NULL for non-DSO files
+
+  if (!is.null(DSOversion)) {
     spc <- new("hyperSpec", spc = data$data)
     spc$spcname <- spcname
-    spc@wavelength <- as.vector(data[["axisscale"]][[2]][[1]])  #get wavelengths
-    spc@label$.wavelength <- data[["axisscale"]][[4]][[1]][[1]] #get units for x-axis
-  }
-  else
-  {
-    #non-DSO file
-    #data is found in [[3]]
-    spc <-  new("hyperSpec", spc = data[[3]])
-    #axisscale is [[4]]
-    spc@wavelength<-as.vector(data[[4]][[2]][[1]])    #get wavelengths
-    spc@label$.wavelength <- data[[4]][[4]][[1]][[1]] #get units for x-axis
+    spc@wavelength <- as.vector(data[["axisscale"]][[2]][[1]]) # get wavelengths
+    spc@label$.wavelength <- data[["axisscale"]][[4]][[1]][[1]] # get units for x-axis
+  } else {
+    # non-DSO file
+    # data is found in [[3]]
+    spc <- new("hyperSpec", spc = data[[3]])
+    # axisscale is [[4]]
+    spc@wavelength <- as.vector(data[[4]][[2]][[1]]) # get wavelengths
+    spc@label$.wavelength <- data[[4]][[4]][[1]][[1]] # get units for x-axis
   }
   ## consistent file import behavior across import functions
   .spc_io_postprocess_optional(spc, file)
