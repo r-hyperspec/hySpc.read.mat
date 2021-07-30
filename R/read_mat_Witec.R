7ea64519cf8d0504eab56d21ff1d99707072b75d
Vilmantas Gėgžna <GegznaV@gmail.com>
2021-07-30 10:51:56 +0300

    Revert "Revert "Merge branch 'develop' of https://github.com/GregoryHuff/hySpc.read.mat into develop""
    This reverts commit 961d522584b7c3834ce10f3e03bbc662c23700ed.

--- R/read_mat_Witec.R
+++ R/read_mat_Witec.R
@@ -1,16 +1,16 @@
-
-#' Read `.mat` file into `hyperSpec` object
+#' Import Matlab (`.mat`) file exported by Witec Project software
 #'
+#' Read `.mat` by Witec Project software file into `hyperSpec` object.
 #'
-#' @param file File name.
+#' @param file Path to file or connection from which to import
 #'
-# @concept io
-#' @concept moved to hySpc.read.txt
-#' @concept moved to hySpc.read.mat
+#' @importClassesFrom hyperSpec hyperSpec
+#' @importFrom methods new
+#' @importFrom utils packageDescription
+#' @import hyperSpec
 #'
-#' @importFrom utils maintainer
 #' @export
-
+#'
 read_mat_Witec <- function(file = stop("filename or connection needed")) {
   if (!requireNamespace("R.matlab")) {
     stop("package 'R.matlab' needed.")
@@ -21,37 +21,37 @@ read_mat_Witec <- function(file = stop("filename or connection needed")) {
   if (length(data) > 1L) {
     stop(
       "Matlab file contains more than 1 object. This should not happen.\n",
-      "If it is nevertheless a WITec exported .mat file, please contact the ",
-      "maintainer (", maintainer("hyperSpec"), ") with\n",
-      "- output of `sessionInfo ()` and\n",
+      "If it is nevertheless a WITec exported .mat file, ",
+      "please open an issue at ",
+      packageDescription("hySpc.read.mat")$BugReports,
+      " with: \n",
+      "- output of `sessionInfo ()` and \n",
       "- an example file"
     )
   }
   spcname <- names(data)
-  #peel off outer layer of data structure, which only contains filename
   data <- data[[1]]
-  #WITec software can export Matlab files in two different formats
-  #the first is the so called DSO format, the other they simply call the Matlab format
-  #The DSO format has more fields, many of which are empty
-  #the names of the fields are not imported correctly in R ("axisscale" for example) when using the non-DSO format
+  # WITec software can export Matlab files in two different formats the first
+  # is the so called DSO format, the other they simply call the Matlab format.
+  # The DSO format has more fields, many of which are empty.
+  # The names of the fields are not imported correctly in R ("axisscale"
+  # for example) when using the non-DSO format.
 
-  #check whether it is DSO or not
-  DSOversion <- data[["datasetversion"]] #this will be NULL for non-DSO files
-  if (!is.null(DSOversion))
-  {
+  # Check whether it is DSO or not
+  DSOversion <- data[["datasetversion"]] # this will be NULL for non-DSO files
+
+  if (!is.null(DSOversion)) {
     spc <- new("hyperSpec", spc = data$data)
     spc$spcname <- spcname
-    spc@wavelength <- as.vector(data[["axisscale"]][[2]][[1]])  #get wavelengths
-    spc@label$.wavelength <- data[["axisscale"]][[4]][[1]][[1]] #get units for x-axis
-  }
-  else
-  {
-    #non-DSO file
-    #data is found in [[3]]
-    spc <-  new("hyperSpec", spc = data[[3]])
-    #axisscale is [[4]]
-    spc@wavelength<-as.vector(data[[4]][[2]][[1]])    #get wavelengths
-    spc@label$.wavelength <- data[[4]][[4]][[1]][[1]] #get units for x-axis
+    spc@wavelength <- as.vector(data[["axisscale"]][[2]][[1]]) # get wavelengths
+    spc@label$.wavelength <- data[["axisscale"]][[4]][[1]][[1]] # get units for x-axis
+  } else {
+    # non-DSO file
+    # data is found in [[3]]
+    spc <- new("hyperSpec", spc = data[[3]])
+    # axisscale is [[4]]
+    spc@wavelength <- as.vector(data[[4]][[2]][[1]]) # get wavelengths
+    spc@label$.wavelength <- data[[4]][[4]][[1]][[1]] # get units for x-axis
   }
   ## consistent file import behavior across import functions
   .spc_io_postprocess_optional(spc, file)
@@ -60,7 +60,10 @@ read_mat_Witec <- function(file = stop("filename or connection needed")) {
 #' @import hySpc.testthat
 #' @import testthat
 hySpc.testthat::test(read_mat_Witec) <- function() {
-  time_series_Witec <- system.file("extdata/mat.Witec", "time-series.mat", package = "hySpc.read.mat")
+  time_series_Witec <- system.file(
+    "extdata/mat.Witec", "time-series.mat",
+    package = "hySpc.read.mat"
+  )
 
   # unit tests for `read_mat_Witec` itself
   ##################################
